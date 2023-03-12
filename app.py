b2308e70f3ca7c297f58458ff89dd2ead9526ee4
anniechakraborty <the.demented.genius29@gmail.com>
2023-03-13 02:59:01 +0530

    created a data file to contain the array of articles to be displayed. created route for the articles page and every individual article

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 # __name__ is the placeholder for the current module (App.py)
+
+articles_array = Articles()
 
 @app.route('/')
 def index():
@@ -10,6 +13,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = articles_array)
+
+@app.route('/articleDetails/<string:id>/')
+def articleDetails(id):
+    return render_template('articleDetails.html', id = id)
 
 if __name__ == '__main__':
     # script which will be executed
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'content' : 'Lorem ipsum dolor sit amet, consectetur adip',
+            'author' : 'Annie Chakraborty',
+            'created_date' : '12 March 2023'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'content' : 'Lorem ipsum dolor sit amet, consectetur adip. Article',
+            'author' : 'Ann Chakraborty',
+            'created_date' : '14 March 2023'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'content' : 'Lorem ipsum dolor sit amet, consectetur adip',
+            'author' : 'Annie C',
+            'created_date' : '16 March 2023'
+        }
+    ]
+    return articles
