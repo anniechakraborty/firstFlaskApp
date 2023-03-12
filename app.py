a6f4634ca1236fde6ffc69881781513eab7e956a
anniechakraborty <the.demented.genius29@gmail.com>
2023-03-12 23:45:41 +0530

    added about page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     # script which will be executed
     app.run(debug=True)
